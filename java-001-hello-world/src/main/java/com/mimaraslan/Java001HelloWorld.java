281b899aa6232e527587d89239c130c33f7ccab3
Mimar Aslan <mimaraslan@gmail.com>
2024-03-02 14:58:59 -0500

    Hello World
    Hello World

---
+++ java-001-hello-world/src/main/java/com/mimaraslan/Java001HelloWorld.java
@@ -0,0 +1,11 @@
+package com.mimaraslan;
+
+public class Java001HelloWorld {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World! ");
+        
+    }
+    
+}
