10c3407deb2b73456113d4480d672b4e88bd17c6
Mimar Aslan <mimaraslan@gmail.com>
2024-03-03 11:38:37 -0500

    Variables değişkenler

--- java-002-variables/src/main/java/com/mimaraslan/AppMain.java
+++ java-002-variables/src/main/java/com/mimaraslan/AppMain.java
@@ -5,5 +5,19 @@ public class AppMain {
 
         System.out.println("Hello world!");
 
+        int sayi1 = 15;
+        int sayi2 = 35;
+        int sonuc = sayi1 + sayi2;
+
+        System.out.println("Sayi1: "+ sayi1);
+        System.out.println("Sayi2: "+ sayi2);
+        System.out.println("Sonuç: "+ sonuc);
+
+        System.out.println("Toplama: "+ (sayi1 + sayi2));
+        System.out.println("Çıkarma: " + (sayi2 - sayi1));
+        System.out.println("Bölme: "+ (sayi1 / sayi2));
+        System.out.println("Çarpma: "+ (sayi1 * sayi2));
+
+
     }
 }
---
+++ java-003-loops/src/main/java/com/mimaraslan/AppMain.java
@@ -0,0 +1,10 @@
+package com.mimaraslan;
+
+public class AppMain {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
