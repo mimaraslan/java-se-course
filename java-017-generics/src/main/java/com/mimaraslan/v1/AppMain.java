5ea077d431bcdbfdce30c86fc1e6ea751d18ec3d
Mimar Aslan <mimaraslan@gmail.com>
2024-04-20 11:58:09 -0400

    Generics

---
+++ java-017-generics/src/main/java/com/mimaraslan/v1/AppMain.java
@@ -0,0 +1,21 @@
+package com.mimaraslan.v1;
+
+import java.util.ArrayList;
+
+public class AppMain {
+    public static void main(String[] args) {
+
+        ArrayList   myArrayList1 = new ArrayList();
+
+        myArrayList1.add("A");
+        myArrayList1.add(10);
+        myArrayList1.add(true);
+
+        ArrayList <String>   myArrayList2 = new ArrayList();
+        myArrayList2.add("B");
+
+
+        PersonelList <String> myPersonelList = new PersonelList();
+
+    }
+}
---
+++ java-017-generics/src/main/java/com/mimaraslan/v1/PersonelList.java
@@ -0,0 +1,4 @@
+package com.mimaraslan.v1;
+
+public class PersonelList {
+}
