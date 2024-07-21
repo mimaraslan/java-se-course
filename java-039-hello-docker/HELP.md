7d788fda390fc12c4593d744a8a155b5a7fcdf14
Mimar Aslan <mimaraslan@gmail.com>
2024-07-21 12:25:09 +0300

    Microservices
    Microservices

---
+++ java-039-hello-docker/HELP.md
@@ -0,0 +1,3 @@
+cd C:\Users\YOUR_USER_NAME\Documents\workspace\java-se-course\java-039-hello-docker
+
+docker build  --build-arg  JAR_FILE=build/libs/java-039-hello-docker-1.0.0.jar -t  YOUR_DOCKER_USER_NAME/java-039-hello-docker:v001 .
--- java-039-hello-docker/src/main/java/com/mimaraslan/controller/PersonelController.java
+++ java-039-hello-docker/src/main/java/com/mimaraslan/controller/PersonelController.java
@@ -4,17 +4,42 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.time.LocalTime;
 
-//   http:localhost:8080/api/v1
+
+//   http://localhost:8080/api/v1
 
 @RestController
 @RequestMapping("/api/v1")
 public class PersonelController {
 
-    //   http:localhost:8080/api/v1/hello
+  /*
+    //   http://localhost:8080/api/v1/hello
     @GetMapping("/hello")
-   public String getDockerHello(){
-        return "Hello Docker World";
+    public String getDockerHello(){
+        return "Hello Docker World : " + LocalTime.now();
     }
+*/
+
+    //  http://localhost:8080/api/v1/hello
+    @GetMapping("/hello")
+    public String getDockerHello() {
+
+        try {
+
+            // get system name
+            String SystemName = InetAddress.getLocalHost().getHostName();
+
+            // SystemName stores the name of the system
+            return "Hello Docker : " + SystemName  + " : " + LocalTime.now();
+        } catch (Exception E) {
+            System.err.println(E.getMessage());
+        }
+
+
+        return "Hello Docker" ;
+    }
+
 
 }
